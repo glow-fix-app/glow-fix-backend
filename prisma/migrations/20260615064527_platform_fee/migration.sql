f370607ac0e251eaccf3e6255babd42305249381
ahmedeid101 <ahmedgebrel101@gmail.com>
2026-06-15 17:06:19 +0300

    fix: correct migration to remove invalid uuid type change on disputes.booking_id

--- prisma/migrations/20260615064527_platform_fee/migration.sql
+++ prisma/migrations/20260615064527_platform_fee/migration.sql
@@ -1,21 +1,12 @@
 /*
   Warnings:
 
-  - Changed the type of `booking_id` on the `disputes` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+  - Added `platform_fee` to `bookings` table.
+  - Added `client_platform_fee` to `settings` table.
 
 */
--- DropForeignKey
-ALTER TABLE "disputes" DROP CONSTRAINT "disputes_booking_id_fkey";
-
 -- AlterTable
 ALTER TABLE "bookings" ADD COLUMN     "platform_fee" DECIMAL(65,30) NOT NULL DEFAULT 0;
 
 -- AlterTable
-ALTER TABLE "disputes" DROP COLUMN "booking_id",
-ADD COLUMN     "booking_id" UUID NOT NULL;
-
--- AlterTable
 ALTER TABLE "settings" ADD COLUMN     "client_platform_fee" DECIMAL(65,30) NOT NULL DEFAULT 0;
-
--- AddForeignKey
-ALTER TABLE "disputes" ADD CONSTRAINT "disputes_booking_id_fkey" FOREIGN KEY ("booking_id") REFERENCES "bookings"("id") ON DELETE CASCADE ON UPDATE CASCADE;
