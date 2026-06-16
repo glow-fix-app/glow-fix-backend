838df4e23df5f419e100575cf0f2c4a7a50d6193
ahmedeid101 <ahmedgebrel101@gmail.com>
2026-06-16 15:56:26 +0300

    feat: fix code for deploy

--- server/src/modules/diagnostic-reports/diagnostic-reports.events.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DIAGNOSTIC_EVENTS = void 0;
-exports.DIAGNOSTIC_EVENTS = {
-    REPORT_CREATED: 'diagnostic.report_created',
-    CLIENT_RESPONDED: 'diagnostic.client_responded',
-};
